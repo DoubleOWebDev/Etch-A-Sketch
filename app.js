4c879fdc87bf1816252116fab59cbaa90b1c57b1
Osasuyi Osawe <oajo8732@gmail.com>
2022-12-27 16:22:15 -0800

    Tinker with relation of grid size button values

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const container = document.querySelector('.container')
+let container = document.querySelector('.container'); //Declared container
+container.style.display = 'grid' //Set display on container
+let div = document.createElement('div'); //Created JS div
 
-const div = document.createElement('div')
-
-container.appendChild(div)
-
+//Function to create boxes within the container
 const howManySquaresDoYouWant = (gridSize) =>{
-  //Make a prompt that will ask for the grid size
+   gridSize = prompt(`How do you want your Grid Size?`);//Make a prompt that will ask for the grid size
+
   for (let i = 0 ; i <= gridSize; i++){
-    container.appendChild(div)
+    container.appendChild(div);
   }
 }
+
+let gridSizeBtn = document.getElementById('gridSize')
+
+gridSizeBtn.addEventListener('click', howManySquaresDoYouWant);
+
+
